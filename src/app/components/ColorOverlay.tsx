bd5875eb9d7410d35ccd962682fc49fc435c8671
emmanishikawa <emmanishikawa@gmail.com>
2025-06-30 16:24:52 -0700

    fix color overlay z index issue

--- src/app/components/ColorOverlay.tsx
+++ src/app/components/ColorOverlay.tsx
@@ -5,8 +5,8 @@ export default function ColorOverlay() {
     return (
         <>
            <div 
-            className="absolute inset-0 mix-blend-overlay pointer-events-none z-10"
-            style={{ backgroundColor: `rgba(245, 255, 252, 0.35)` }}
+            className="absolute inset-0 mix-blend-overlay pointer-events-none z-9"
+            style={{ backgroundColor: `rgba(255, 240, 255, 0.35)` }}
             />
         </>
     );
